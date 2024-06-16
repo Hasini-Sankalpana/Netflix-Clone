44d3a98472b7cccef87e9e5875094b8b2dbf51a5
Hasini Sankalpana <156077759+Hasini-Sankalpana@users.noreply.github.com>
2024-06-16 17:14:38 +0530

    Display movies data from TMDB API

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,17 +1,33 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './TitleCards.css';
 import cards_data from '../../assets/cards/Cards_data';
 
 
 const TitleCards = ({title, category}) => {
-
+  const [apiData, setApiData] = useState([]);
   const cardsRef = useRef();
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZmNkODljZTI0NGQwZDY1MjcyMDA4MWUyMDZiN2YwZSIsInN1YiI6IjY2NmVjOTgyM2MxNDFmYzY2NDZiYzJmMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tKvKw2inO1nHhnNP1F8tuZopkI8a00S-b66rXEWaGfg'
+    }
+  };
+  
+  
 
 const handleWheel = (event) => {
 event.preventDefault();
 cardsRef.current.scrollLeft += event.deltaY;
 }
 useEffect(() => {
+
+  fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`, options)
+    .then(response => response.json())
+    .then(response => setApiData(response.results))
+    .catch(err => console.error(err));
+
   cardsRef.current.addEventListener('wheel',handleWheel)
 },[])
 
@@ -19,10 +35,10 @@ useEffect(() => {
     <div className='title-cards'>
       <h2>{title?title:"Popular on Netflix"}</h2>
       <div className='card-list' ref={cardsRef}>
-        {cards_data.map( (card, index)=> {
+        {apiData.map( (card, index)=> {
              return <div className="card" key={{index}}>
-              <img src={card.image} alt="" />
-              <p>{card.name}</p>
+              <img src={`https://image.tmdb.org/t/p/w500/` +card.backdrop_path} alt="" />
+              <p>{card.original_title}</p>
              </div>
 
         })}
